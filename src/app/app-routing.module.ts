d25f22fbf462d98f475a282a77961637eb399796
surullisuus <jsfgc@hotmail.com>
2021-12-16 15:15:35 -0500

    cambio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LayoutfullComponent } from './layout/layoutfull/layoutfull.component';
 
-const routes: Routes = [
-  {
-    path:'',
-    redirectTo: '/login',
-    pathMatch: 'full',
-  },
-  {
-    path:'',
-    children: [
-      {
-        path: 'pages',
-        component: LayoutfullComponent,
-        loadChildren: () => import('./layout/layout.module').then(m =>m.LayoutModule),
-      }
-    ]
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
